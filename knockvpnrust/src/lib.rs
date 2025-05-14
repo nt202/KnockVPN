d08b84309f24a4be4c83e1ffdce690c926fcafe8
Timur Kushchanov <timur.kushchanov@yandex.ru>
2025-05-15 00:07:46 +0300

    1

--- knockvpnrust/src/lib.rs
+++ knockvpnrust/src/lib.rs
@@ -1,4 +1,4 @@
-use jni::objects::JClass;
+use jni::objects::{JClass, JString};
 use jni::sys::jint;
 use jni::JNIEnv;
 use jni::sys::jstring;
@@ -19,4 +19,28 @@ pub extern "system" fn Java_com_nt202_knockvpn_VpnActivity_sumFromRust(
     b: jint,
 ) -> jint {
     a + b
+}
+
+#[no_mangle]
+pub extern "system" fn Java_com_nt202_knockvpn_VpnActivity_concatenateStrings(
+    mut env: JNIEnv,
+    _: JClass,
+    str1: JString,  // First Java string
+    str2: JString,  // Second Java string
+) -> jstring {
+    // Convert Java strings to Rust strings
+    let rust_str1: String = env.get_string(&str1)
+        .expect("Failed to get Rust string from Java")
+        .into();
+    let rust_str2: String = env.get_string(&str2)
+        .expect("Failed to get Rust string from Java")
+        .into();
+
+    // Concatenate
+    let combined = format!("{}{}", rust_str1, rust_str2);
+
+    // Convert back to Java string and return as raw `jstring`
+    env.new_string(combined)
+        .expect("Couldn't create Java string!")
+        .into_raw()
 }
